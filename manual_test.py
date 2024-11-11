b5429ac3b38943f5671508425963595cf3f4a875
Robin Gergelyfi <gergrobin@gmail.com>
2024-11-11 21:30:58 +0100

    VCS: test file updated

--- manual_test.py
+++ manual_test.py
@@ -23,9 +23,18 @@ vcs.init()
 # commit, hashed_commit = vcs.create_commit(hashed_tree)
 # vcs.store_blob(commit, hashed_commit)
 
-vcs.stage_files(file_1)
-vcs.show_changed_objects("cd/1837365fe768e6d3a7659316ecd8f38cc4a58e")
+# vcs.show_changed_objects("cd/1837365fe768e6d3a7659316ecd8f38cc4a58e")
 # vcs.get_all_files_in_repo()
-# vcs.make_commit()
+
+vcs.stage_files(file_1)
+vcs.make_commit()
+
+# vcs.display_changed_files()
+
+vcs.display_commit_tree()
+
+# with open(".vcs/objects/36/05903313c2d056718b97105f85409d37c6f417", "r") as f:
+#     cont = f.read()
+#     print(cont)
 
 # vcs.show_vcs_tree()
