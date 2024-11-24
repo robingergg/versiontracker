ab75d35c3dd87a027921622232a13532b090e701
Robin Gergelyfi <gergrobin@gmail.com>
2024-11-24 12:02:08 +0100

    VCS: updated manual test for testing VCS in a ninteractive CLI env.

--- manual_test.py
+++ manual_test.py
@@ -1,40 +1,73 @@
 from main import *
+import cmd2
+
 
 vcs = MyVcs()
 
+stage_parser = cmd2.Cmd2ArgumentParser()
+stage_parser.add_argument('-f', '--files', nargs='+', help='Files to be staged')
 
+show_modified_files_parser = cmd2.Cmd2ArgumentParser()
+show_log_graph_parser = cmd2.Cmd2ArgumentParser()
+
+commit_parser = cmd2.Cmd2ArgumentParser()
+commit_parser.add_argument('-m', '--message')
+
+show_staged_files_parser = cmd2.Cmd2ArgumentParser()
+read_tree_parser = cmd2.Cmd2ArgumentParser()
+
+ammend_parser = cmd2.Cmd2ArgumentParser()
+ammend_parser.add_argument('-m', '--message')
+
+class MainVcs(cmd2.Cmd):
+
+    def do_init(self, args: cmd2.Statement):
+        vcs.init()
+
+    @cmd2.with_argparser(read_tree_parser)
+    def do_read_tree(self):
+        """Reads the tree of a given commit"""
+        
+
+
+    @cmd2.with_argparser(stage_parser)
+    def do_stage_files(self, args):
+        """Stages the selected files"""
+        files = args.files
+        print(f"Files to stage: {files}")
+        for f in files:
+            vcs.stage_file(f)
+
+    @cmd2.with_argparser(show_staged_files_parser)
+    def do_show_staged(self, args):
+        vcs.show_staged_files()
+
+    @cmd2.with_argparser(commit_parser)
+    def do_commit(self, args):
+        msg = args.message if args.message else None 
+        vcs.make_commit(message=msg)
+
+    @cmd2.with_argparser(show_modified_files_parser)
+    def do_show_modified_files(self, args):
+        vcs.display_modified_files()
+
+    @cmd2.with_argparser(show_log_graph_parser)
+    def do_show_log_graph(self, args):
+        vcs.display_commit_tree()
+
+    @cmd2.with_argparser(ammend_parser)
+    def do_ammend(self, args):
+        message = None
+        if args.message:
+            message = args.message
+        vcs.ammend(message)
 
 
 file_1 = "my.txt"
 file_2 = "to.txt"
-vcs.init()
-# blob_1, hashed_blob_1 = vcs.create_blob(file_1)
-# blob_2, hashed_blob_2 = vcs.create_blob(file_2)
-
-# vcs.store_blob(blob_1, hashed_blob_1)
-# vcs.store_blob(blob_2, hashed_blob_2)
-
-# files = [[file_1, hashed_blob_1]]
-# files = [[file_1, hashed_blob_1], [file_2, hashed_blob_2]]
-
-# tree, hashed_tree = vcs.create_tree(files)
-# vcs.store_blob(tree, hashed_tree)
-
-# commit, hashed_commit = vcs.create_commit(hashed_tree)
-# vcs.store_blob(commit, hashed_commit)
-
-# vcs.show_changed_objects("cd/1837365fe768e6d3a7659316ecd8f38cc4a58e")
-# vcs.get_all_files_in_repo()
-
-vcs.display_changed_files()
-vcs.stage_files(file_1)
-vcs.make_commit()
 
 
-vcs.display_commit_tree()
-
-# with open(".vcs/objects/36/05903313c2d056718b97105f85409d37c6f417", "r") as f:
-#     cont = f.read()
-#     print(cont)
-
-# vcs.show_vcs_tree()
+if __name__ == '__main__':
+    import sys
+    c = MainVcs()
+    sys.exit(c.cmdloop())
